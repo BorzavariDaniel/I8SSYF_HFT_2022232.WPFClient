253e3adbd3a2c34c6a8bfe16b0820dbd435bb9ce
BorzavariDaniel <dani.borzavari@gmail.com>
2023-05-31 19:06:18 +0200

    NonCruds working

--- I8SSYF_HFT_2021221.JSClient/Program.cs
+++ I8SSYF_HFT_2021221.JSClient/Program.cs
@@ -1,5 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 app.MapGet("/", () => "Hello World!");
 
